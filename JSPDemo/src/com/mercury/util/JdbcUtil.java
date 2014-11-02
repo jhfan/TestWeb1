11f21386f10da16d2a5dff77358a2524f9d5a665
andyjunzi <zhengjunli36@gmail.com>
2014-11-02 14:45:11 -0500

    Revert "new Connection"
    This reverts commit cf424c16f5f3e7be1ec4dfcedf9bdf30b9d949c2.

--- JSPDemo/src/com/mercury/util/JdbcUtil.java
+++ JSPDemo/src/com/mercury/util/JdbcUtil.java
@@ -1,19 +1,18 @@
 package com.mercury.util;
-import java.sql.Connection;
-
-import javax.naming.Context;
-import javax.naming.InitialContext;
-import javax.sql.DataSource;
+import java.sql.*;
 
 public class JdbcUtil {
-	public Connection getConnection() {
-		Connection conn = null;
-		try {
-			Context initContext = new InitialContext();
-			Context envContext  = (Context)initContext.lookup("java:/comp/env");
-			DataSource ds = (DataSource)envContext.lookup("jdbc/myoracle");
-			conn = ds.getConnection();
-		} catch (Exception e) {
+	private static final String DRIVER="oracle.jdbc.driver.OracleDriver";
+	private static final String URL="jdbc:oracle:thin:@Zhengjun-PC:1521:XE";
+	private static final String USERNAME="user_java";
+	private static final String PASSWORD="l19891013";
+	
+	public static Connection getConnection(){
+		Connection conn=null;
+		try{
+			Class.forName(DRIVER);
+			conn=DriverManager.getConnection(URL, USERNAME, PASSWORD);
+		} catch(Exception e){
 			e.printStackTrace();
 		}
 		return conn;
