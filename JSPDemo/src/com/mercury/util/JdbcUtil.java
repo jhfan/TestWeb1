917eaa5844b8005cb3f629a7f26c249fdbd333c0
andyjunzi <zhengjunli36@gmail.com>
2014-11-02 13:43:49 -0500

    change JdbcUtil

--- JSPDemo/src/com/mercury/util/JdbcUtil.java
+++ JSPDemo/src/com/mercury/util/JdbcUtil.java
@@ -3,9 +3,9 @@ import java.sql.*;
 
 public class JdbcUtil {
 	private static final String DRIVER="oracle.jdbc.driver.OracleDriver";
-	private static final String URL="jdbc:oracle:thin:@jhfanPC:1521:XE";
-	private static final String USERNAME="java";
-	private static final String PASSWORD="12345";
+	private static final String URL="jdbc:oracle:thin:@Zhengjun-PC:1521:XE";
+	private static final String USERNAME="user_java";
+	private static final String PASSWORD="l19891013";
 	
 	public static Connection getConnection(){
 		Connection conn=null;
